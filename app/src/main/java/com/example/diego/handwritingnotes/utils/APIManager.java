ae4efdcf395cded16764580221c7703e51a6bf58
dbeckdorf <dbeckdorf@miuandes.cl>
2018-06-10 18:20:15 -0400

    Added azure handwriting recognition service dependencies and mock functions

---
+++ app/src/main/java/com/example/diego/handwritingnotes/utils/APIManager.java
@@ -0,0 +1,125 @@
+package com.example.diego.handwritingnotes.utils;
+
+import android.graphics.Bitmap;
+
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.Header;
+import org.json.JSONObject;
+
+/**
+ * Created by diego on 10-06-2018.
+ */
+
+public class APIManager {
+
+    private static final String subscriptionKey = "<Subscription Key>";
+
+    private static final String uriBase =
+            "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/recognizeText";
+
+    private static final String imageToAnalyze =
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/" +
+                    "Cursive_Writing_on_Notebook_paper.jpg/800px-Cursive_Writing_on_Notebook_paper.jpg";
+
+    public String processAPIResponse() {
+        return "Testo de la imagen";
+    }
+
+    public void requestAPIProcess(Bitmap image) {
+        CloseableHttpClient httpTextClient = HttpClientBuilder.create().build();
+        CloseableHttpClient httpResultClient = HttpClientBuilder.create().build();;
+
+        try {
+            // This operation requires two REST API calls. One to submit the image
+            // for processing, the other to retrieve the text found in the image.
+
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Request parameter.
+            // Note: The request parameter changed for APIv2.
+            // For APIv1, it is "handwriting", "true".
+            builder.setParameter("mode", "Handwritten");
+
+            // Prepare the URI for the REST API call.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity requestEntity =
+                    new StringEntity("{\"url\":\"" + imageToAnalyze + "\"}");
+            request.setEntity(requestEntity);
+
+            // Make the first REST API call to detect the text.
+            HttpResponse response = httpTextClient.execute(request);
+
+            // Check for success.
+            if (response.getStatusLine().getStatusCode() != 202) {
+                // Format and display the JSON error message.
+                HttpEntity entity = response.getEntity();
+                String jsonString = EntityUtils.toString(entity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Error:\n");
+                System.out.println(json.toString(2));
+                return;
+            }
+
+            // Stores the URI where you can get the text recognition operation result.
+            String operationLocation = null;
+
+            // 'Operation-Location' in the response contains the URI to
+            // retrieve the recognized text.
+            Header[] responseHeaders = response.getAllHeaders();
+            for (Header header : responseHeaders) {
+                if (header.getName().equals("Operation-Location")) {
+                    operationLocation = header.getValue();
+                    break;
+                }
+            }
+
+            if (operationLocation == null) {
+                System.out.println("\nError retrieving Operation-Location.\nExiting.");
+                System.exit(1);
+            }
+
+            // Note: The response may not be immediately available. Handwriting
+            // recognition is an asynchronous operation, which takes a variable
+            // amount of time dependent on the length of the text analyzed. You
+            // may need to wait or retry the Get operation.
+
+            System.out.println("\nHandwritten text submitted.\n" +
+                    "Waiting 10 seconds to retrieve the recognized text.\n");
+            Thread.sleep(10000);
+
+            // Make the second REST API call and get the response.
+            HttpGet resultRequest = new HttpGet(operationLocation);
+            resultRequest.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponse resultResponse = httpResultClient.execute(resultRequest);
+            HttpEntity responseEntity = resultResponse.getEntity();
+
+            if (responseEntity != null) {
+                // Format and display the JSON response.
+                String jsonString = EntityUtils.toString(responseEntity);
+                JSONObject json = new JSONObject(jsonString);
+                System.out.println("Text recognition result response: \n");
+                System.out.println(json.toString(2));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
