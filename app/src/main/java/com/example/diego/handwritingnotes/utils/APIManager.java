41a1389ef0f8f4d67d49a846cd7df1f693dc7b78
dbeckdorf <dbeckdorf@miuandes.cl>
2018-06-30 21:26:37 -0400

    Refactor APIManager for Volley implementation

--- app/src/main/java/com/example/diego/handwritingnotes/utils/APIManager.java
+++ app/src/main/java/com/example/diego/handwritingnotes/utils/APIManager.java
@@ -1,8 +1,19 @@
 package com.example.diego.handwritingnotes.utils;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -35,6 +46,46 @@ public class APIManager {
         return "Testo de la imagen";
     }
 
+    public boolean sendImage(Context context, Bitmap image) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, uriBase,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //TODO call for asyn service to request recognition text from second uri
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO notify image was not propertly sended.
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("mode", "Handwritten");
+                //TODO set local image on params
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/octet-stream");
+                headers.put("Ocp-Apim-Subscription-Key", subscriptionKey);
+                return headers;
+            }
+        };
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        return true;
+    }
 
     public void requestAPIProcess(Bitmap image) {
         CloseableHttpClient httpTextClient = HttpClientBuilder.create().build();
@@ -56,7 +107,7 @@ public class APIManager {
             HttpPost request = new HttpPost(uri);
 
             // Request headers.
-            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Content-Type", "application/json"); //TODO use application/octet-stream instead
             request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
 
             // Request body.
